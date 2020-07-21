10d5a7cef5835d10ac5c19ff9878152d01b5439f
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-07-21 16:26:41 +0700

    update productlist

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,7 +1,12 @@
 export class Product {
-    // public rating: number;
-    public available: boolean;
-    public imageUrl: string;
-  
-    constructor(public code: string, public name: string, public price: number, public rating: number) {}
+  // public rating: number;
+  // public available: boolean;
+  public imageUrl: string;
+
+  constructor(
+    public code: string,
+    public name: string,
+    public price: number,
+    public rating: number
+  ) {}
 }
