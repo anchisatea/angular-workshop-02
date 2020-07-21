2d68f38067ede430474968a8ce1140115b393c6e
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-07-21 15:05:51 +0700

    cm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'list', component: ProductlistComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
