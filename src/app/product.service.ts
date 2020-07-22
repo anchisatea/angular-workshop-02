a06e476ebd42d371f5152cc0a9718156ce0db231
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-07-22 23:09:39 +0700

    cm

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,9 +12,9 @@ export class ProductService {
   public product: Product;
 
   getAllProduct(): Observable<Product[]> {
-    const p1 = new Product('1101', 'Name 01', 1000.0, 1, true);
-    const p2 = new Product('1102', 'Name 02', 2500.5, 2, true);
-    const p3 = new Product('1103', 'Name 03', 3500.0, 3, true);
+    const p1 = new Product('1101', 'Name 01', 1000.0, 1, true, 'xxx');
+    const p2 = new Product('1102', 'Name 02', 2500.5, 2, true, 'xxx');
+    const p3 = new Product('1103', 'Name 03', 3500.0, 3, true, 'xxx');
     return of([p1, p2, p3]);
     // return this.http.get<Product[]>('http://165.22.255.58:3000/products');
   }
