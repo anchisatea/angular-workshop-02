5adec87d9ab51538a193dc49cd871465ee73308a
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-07-22 17:08:01 +0700

    cm

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from './models/product';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
