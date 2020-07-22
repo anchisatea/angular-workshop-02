332863b1e61a821c933e3bca75929f26860e6a34
Anchisa Teachalikhidkul <anchisatea@mathregit.group>
2020-07-22 23:59:31 +0700

    cm

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,22 +12,8 @@ export class ProductService {
   public product: Product;
 
   getAllProduct(): Observable<Product[]> {
-    const p1 = new Product(
-      '1101',
-      'Facial cleanser 01',
-      1000.0,
-      1,
-      true,
-      'https://www.kiehls.co.th/media/catalog/product/cache/2/thumbnail/4dc2e3274ef0740eb55a46bfb4da7e12/3/6/3605971864865.1583814297.jpg'
-    );
-    const p2 = new Product(
-      '1102',
-      'Repair cream 02',
-      2500.5,
-      2,
-      true,
-      'https://www.eucerin.co.th/~/media/eucerin/local/th/home-stage-teasers/2020/202003-product-image-3d-packshot/ph5/ph5_facial-cleanser/ph5_facial-cleanser.png?h=240&mw=290&w=290&rh=0&devicepixelratio=2&hash=67B54CB3188E00AC855A92E9E179BA05'
-    );
+    const p1 = new Product('1101', 'Facial cleanser 01', 1000.0, 1, true, 'xx');
+    const p2 = new Product('1102', 'Repair cream 02', 2500.5, 2, true, 'xx');
     const p3 = new Product(
       '1103',
       'Perfume 03',
